a901ebbb260b2c31bd325a5fb2d69af932ef8835
kerinshah3 <kerinshah3@gmail.com>
2024-01-03 19:19:48 -0500

    form validation with yup and updated readme

--- src/component/react-hook-form/SignUp.js
+++ src/component/react-hook-form/SignUp.js
@@ -2,10 +2,22 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import 'materialize-css/dist/css/materialize.min.css'
 import { useForm } from "react-hook-form";
-export default function SignUp(){
+import { yupResolver } from "@hookform/resolvers/yup"
+import * as yup  from 'yup'
 
-    const {register , handleSubmit} = useForm()
+export default function SignUp(){
+    const schema = yup.object().shape({
+        name: yup.string().required().min(3),
+        email: yup.string().email().required(),
+        age: yup.number().positive().required().integer().min(15),
+        password: yup.string().min(8).max(20).required(),
+        conformPassword: yup.string().oneOf([yup.ref("password"),null]).required()
+    })
+    const {register , handleSubmit, formState: { errors }} = useForm({
+        resolver: yupResolver(schema)
+    })
     function onSubmit(data) {
+        // Database / Backend Call will come here
         console.log(data)
     }
 
@@ -19,10 +31,16 @@ export default function SignUp(){
                         placeholder="Name"
                         {...register("name")}
                     />
+                    <p className="red-text">{errors.name?.message}</p>
                     <input
                         type="text"
                         placeholder="Email"
                         {...register("email")}
+                    />
+                    <input
+                        type="text"
+                        placeholder="Age"
+                        {...register("age")}
                     />
                     <input
                         type="password"
@@ -32,7 +50,7 @@ export default function SignUp(){
                     <input
                         type="password"
                         placeholder="Conform Password"
-                        {...register("conform password")}
+                        {...register("conformPassword")}
                     />
                     <input
                         className="btn waves-effect waves-light purple lighten-2"
